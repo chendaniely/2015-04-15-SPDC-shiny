6a0147acf266afd2c6c7c803e6ba1f6e2f92782e
Daniel Chen <Maveri2k@yahoo.com>
2015-04-15 14:20:46 -0400

    use kwargs instead of args in the function call.  Easier to understand what it means this way

--- lessons/05-shiny_application_ebola/ui.R
+++ lessons/05-shiny_application_ebola/ui.R
@@ -5,10 +5,14 @@ shinyUI(fluidPage(
     sidebarLayout(
         sidebarPanel("Interactive plot components",
                      uiOutput("countriesList"),
-                     radioButtons("date_offset", "Date range:", c("Relative"="relative", "Absolute"="absolute")),
-                     checkboxInput("log", "Plot y-axis on log scale")
+                     radioButtons(inputId = "date_offset",
+                                  label = "Date range:",
+                                  choices = c("Relative"="relative", "Absolute"="absolute")),
+                     checkboxInput(inputId = "log",
+                                   label = "Plot y-axis on log scale")
         ),
-        mainPanel(p("This graphs the cases and deaths of each country and normalizes the onset dates all to '0' so countries can be compared"),
+        mainPanel(p("This graphs the cases and deaths of each country and
+                    normalizes the onset dates all to '0' so countries can be compared"),
                   "Data was all taken from Caitlin River's 'ebola' repository",
                   a('here', href = 'https://github.com/cmrivers/ebola'),
 
